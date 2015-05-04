67512562ac230274152ea57e52c9b63b80cb18af
René Sprotte <rene.sprotte@provideal.net>
2015-05-04 15:52:03 +0200

    Tune reference implementation.

--- benchmarks/mab2_document_query.rb
+++ benchmarks/mab2_document_query.rb
@@ -2,20 +2,20 @@ require "metacrunch/mab2"
 require "benchmark/ips"
 
 Benchmark.ips do |x|
-  x.config(time: 5, warmup: 2)
+  x.config(time: 10, warmup: 2)
 
   @xml      = File.read(File.join(Pathname.new(__dir__), "..", "spec", "assets", "aleph_mab_xml", "file1.xml"))
   @document = Metacrunch::Mab2::Document.from_aleph_mab_xml(@xml)
 
   x.report("reference") do
-    data_fields_struct = @document.send("data_fields_struct")
+    data_fields_struct = @document.send(:data_fields_struct)
     data_fields_set = data_fields_struct["PPE"]
-    data_fields = data_fields_set.send("__getobj__")
+    data_fields = data_fields_set.__getobj__
 
     data_fields.each do |data_field|
-      sub_fields_struct = data_field.send("sub_fields_struct")
+      sub_fields_struct = data_field.send(:sub_fields_struct)
       sub_fields_set = sub_fields_struct["p"]
-      sub_fields = sub_fields_set.send("__getobj__")
+      sub_fields = sub_fields_set.__getobj__
 
       sub_fields.each do |sub_field|
         sub_field.value
