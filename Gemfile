d2b79b79e312564b63d6bdb93eb6e227ec65664f
René Sprotte <rene.sprotte@provideal.net>
2015-05-04 14:29:01 +0200

    Add benchmark for query optimization.

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ gemspec
 gem "metacrunch", ">= 2.1.0.pre", github: "ubpb/metacrunch", branch: "master"
 
 gem "rake"
-gem "pry-byebug", "~> 3.1.0"
-gem "rspec",      "~> 3.2.0"
+gem "pry-byebug",    "~> 3.1.0"
+gem "rspec",         "~> 3.2.0"
+gem "benchmark-ips", "~> 2.1"
---
+++ benchmarks/mab2_document_query.rb
@@ -0,0 +1,32 @@
+require "metacrunch/mab2"
+require "benchmark/ips"
+
+Benchmark.ips do |x|
+  x.config(time: 5, warmup: 2)
+
+  @xml      = File.read(File.join(Pathname.new(__dir__), "..", "spec", "assets", "aleph_mab_xml", "file1.xml"))
+  @document = Metacrunch::Mab2::Document.from_aleph_mab_xml(@xml)
+
+  x.report("reference") do
+    data_fields_struct = @document.send("data_fields_struct")
+    data_fields_set = data_fields_struct["PPE"]
+    data_fields = data_fields_set.send("__getobj__")
+
+    data_fields.each do |data_field|
+      sub_fields_struct = data_field.send("sub_fields_struct")
+      sub_fields_set = sub_fields_struct["p"]
+      sub_fields = sub_fields_set.send("__getobj__")
+
+      sub_fields.each do |sub_field|
+        sub_field.value
+      end
+    end
+  end
+
+  x.report("implementation") do
+    @document.values(data_field: "PPE", sub_field: "p")
+  end
+
+  x.compare!
+end
+
