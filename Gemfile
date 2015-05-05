ce97abc11ec0e723821ca286dfd4c4f674ed236e
René Sprotte <rene.sprotte@provideal.net>
2015-05-05 17:53:43 +0200

    For benchmarking lets compare to mabmapper.
    When comparing parsing speed, metacrunch appears to be ~70 times faster
    than mabmapper. WOW.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gemspec
 
 gem "metacrunch", ">= 2.1.0.pre", github: "ubpb/metacrunch", branch: "master"
 
+gem "mabmapper", github: "ubpb/mabmapper", branch: "master"
+
 gem "rake"
 gem "pry-byebug",    "~> 3.1.0"
 gem "rspec",         "~> 3.2.0"
--- benchmarks/mab2_document_parsing.rb
+++ benchmarks/mab2_document_parsing.rb
@@ -1,12 +1,22 @@
 require "metacrunch/mab2"
 require "benchmark/ips"
 
+require "mabmapper"
+require "mabmapper/aleph_mab_xml_engine"
+class EmptyMabmapperEngine < Mabmapper::Engine
+  document_class Mabmapper::MabXml::Document
+end
+
 Benchmark.ips do |x|
   x.config(time: 5, warmup: 2)
 
   @xml = File.read(File.join(Pathname.new(__dir__), "..", "spec", "assets", "aleph_mab_xml", "file1.xml"))
 
-  x.report("implementation") do
+  x.report("mabmapper") do
+    @document = EmptyMabmapperEngine.new.process("dummy.xml", @xml)
+  end
+
+  x.report("metacrunch") do
     @document = Metacrunch::Mab2::Document.from_aleph_mab_xml(@xml)
   end
 
