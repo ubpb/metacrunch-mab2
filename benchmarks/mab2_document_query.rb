4e0dc44d513c2fe4fd92874d0f524670ce1668ee
René Sprotte <rene.sprotte@provideal.net>
2015-05-05 11:38:19 +0200

    5 seconds runtime is enough.

--- benchmarks/mab2_document_query.rb
+++ benchmarks/mab2_document_query.rb
@@ -2,7 +2,7 @@ require "metacrunch/mab2"
 require "benchmark/ips"
 
 Benchmark.ips do |x|
-  x.config(time: 10, warmup: 2)
+  x.config(time: 5, warmup: 2)
 
   @xml      = File.read(File.join(Pathname.new(__dir__), "..", "spec", "assets", "aleph_mab_xml", "file1.xml"))
   @document = Metacrunch::Mab2::Document.from_aleph_mab_xml(@xml)
