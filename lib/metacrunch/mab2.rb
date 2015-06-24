8cf0b42434b4f54bafbb40e8efb7b2e7bff97109
René Sprotte <rene.sprotte@provideal.net>
2015-06-24 13:48:40 +0200

    Add a simple builder DSL to quickly build mab2 documents.

--- lib/metacrunch/mab2.rb
+++ lib/metacrunch/mab2.rb
@@ -12,5 +12,6 @@ module Metacrunch
     require_relative "./mab2/document"
     require_relative "./mab2/cli"
     require_relative "./mab2/aleph_mab_xml_document_factory"
+    require_relative "./mab2/builder"
   end
 end
---
+++ lib/metacrunch/mab2/builder.rb
@@ -0,0 +1,51 @@
+require_relative "./document"
+
+module Metacrunch
+  module Mab2
+    class Builder
+
+      attr_reader :document
+
+      def self.build(&block)
+        builder = new
+        builder.instance_eval(&block)
+      end
+
+      def initialize
+        @document = Document.new
+      end
+
+      def controlfield(tag, values)
+        controlfield = Document::Controlfield.new(tag, values)
+        @document.add_controlfield(controlfield)
+        @document
+      end
+
+      def datafield(tag, ind1:nil, ind2:nil, &block)
+        datafield = Document::Datafield.new(tag, ind1: ind1, ind2: ind2)
+        @document.add_datafield(datafield)
+
+        if block_given?
+          subfield_builder = SubfieldBuilder.new(datafield)
+          subfield_builder.instance_eval(&block)
+        end
+
+        @document
+      end
+
+
+      class SubfieldBuilder
+        def initialize(datafield)
+          @datafield = datafield
+        end
+
+        def subfield(code, value)
+          subfield = Document::Datafield::Subfield.new(code, value)
+          @datafield.add_subfield(subfield)
+          subfield
+        end
+      end
+
+    end
+  end
+end
